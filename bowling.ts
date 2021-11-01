85b108e6ac3ce69758160f24c5505febbe7d9f0f
Ben Johnson <ben.johnson@jupiterone.com>
2021-11-01 10:58:32 -0400

    Initial commit

---
+++ bowling.ts
@@ -0,0 +1,9 @@
+export class Bowling {
+  public roll(pins: unknown): unknown {
+    throw new Error("Replace with implementation");
+  }
+
+  public score(): unknown {
+    throw new Error("Replace with implementation");
+  }
+}
