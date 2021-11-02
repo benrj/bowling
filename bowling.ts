7f48b95e33dffce94e2792bb637aeda227887b7e
Carlos Filoteo <c.filoteo12@gmail.com>
2021-11-02 10:31:51 -0600

    WIP: session 1

--- bowling.ts
+++ bowling.ts
@@ -1,8 +1,14 @@
 export class Bowling {
   private currentScore = 0;
+  private currentFrame = [];
 
   public roll(pins: number) {
-    this.currentScore += pins;
+    this.currentFrame.push(pins);
+    // decide if frame is over
+    
+    // score the frame and add to currentScore
+    // this.currentScore += pins;
+    // reset the frame
   }
 
   public score(): unknown {
